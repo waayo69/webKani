f2daf749105a18f2bf3ed0bf530efce3fdcad128
ACT-STUDENT <ACT-STUDENT@LAB1-PC22>
2025-03-19 15:55:43 +0800

    Add project files.

---
+++ webKani/Controllers/ItemsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webKani.Data;
+using webKani.Models;
+
+namespace webKani.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly MyAppContext _context;
+
+        public ItemsController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+            return View(item);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+
+}
---
+++ webKani/Data/MyAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer;
+using webKani.Models;
+
+namespace webKani.Data
+{
+    public class MyAppContext:DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
